2970763c2b5706a6bcbc872fee7467fe23356ace
MHE <mikael.hernrup@lundalogik.se>
2017-03-06 07:40:02 +0100

    fixes broken cygwin aliases due to removed symlink to c drive

--- .zsh/alias.cygwin.zsh
+++ .zsh/alias.cygwin.zsh
@@ -24,8 +24,8 @@ alias bgd="/cygdrive/c/python34/scripts/mtc set solarized-dark"
 # apps
 alias subl="$program_files/Sublime\ Text\ 3/sublime_text.exe"
 alias pycharm="cygstart $program_files_x86/JetBrains/PyCharm\ 2016.1.2/bin/pycharm.exe $(cygpath -w -m ${$(pwd)}) 2>/dev/null &"
-alias node="console.exe node"
-alias vagrant="console.exe /c/HashiCorp/Vagrant/bin/vagrant.exe"
+alias node="winpty node"
+alias vagrant="winpty /cygdrive/c/HashiCorp/Vagrant/bin/vagrant.exe"
 
 # kill stuff
 alias killpython="taskkill /F /IM python.exe /T"
@@ -36,8 +36,8 @@ alias killlime='killall java.exe && killpython && killall node.exe'
 # Lime stuff
 lime_dir="/c/src"
 alias cdlimedata="cd $c/ProgramData/Lundalogik/LIME\ Pro\ Server/"
-alias lisaserver="console.exe $lime_dir/limeworld/serveradmin/Server/Lundalogik.Lisa.Service.ConsoleHost/bin/Debug/LisaConsole.exe"
+alias lisaserver="winpty $lime_dir/limeworld/serveradmin/Server/Lundalogik.Lisa.Service.ConsoleHost/bin/Debug/LisaConsole.exe"
 alias tngclient="$browser \"https://localhost/\""
 alias desktopclient="open $lime_dir/desktop-client/src/Debug/Lime.exe"
-alias lf='console.exe limefu'
+alias lf='winpty limefu'
 
