459caf9ee168e1b24244684c98311167d3a663c1
Mikael Hernrup <mikael@hernrup.se>
2018-06-18 13:07:51 +0200

    fix broken install script

--- install.sh
+++ install.sh
@@ -5,15 +5,19 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 ln -sbfv $SCRIPTPATH/.gitignore ~/.gitignore
 ln -sbfv $SCRIPTPATH/.gitconfig ~/.gitconfig
 ln -sbfv $SCRIPTPATH/.agignore ~/.agignore
-ln -sbfv $SCRIPTPATH/.vimrc ~/.vimrc
 ln -sbfv $SCRIPTPATH/.minttyrc ~/.minttyrc
 ln -sbfv $SCRIPTPATH/.tmux.conf ~/.tmux.conf
-ln -sbfv $SCRIPTPATH/vim ~/.vim
-ln -sbfv $SCRIPTPATH/vim ~/.config/nvim
-ln -sbfv $SCRIPTPATH/bin ~/bin
-ln -sbfv $SCRIPTPATH/.zshrc ~/.zshrc
 ln -sbfv $SCRIPTPATH/.pdbrc ~/.pdbrc
-ln -sbfv $SCRIPTPATH/zsh ~/.zsh
+
+ln -sbfv $SCRIPTPATH/.vimrc ~/.vimrc
+ln -sbfvn $SCRIPTPATH/vim ~/.vim
+ln -sbfvn $SCRIPTPATH/vim ~/.config/nvim
+
+ln -sbfvn $SCRIPTPATH/bin ~/bin
+
+ln -sbfv $SCRIPTPATH/.zshrc ~/.zshrc
+ln -sbfvn $SCRIPTPATH/zsh ~/.zsh
+
 ln -sbfv $SCRIPTPATH/.ctags ~/.ctags
 
 if [ ! -f ~/.gitconfig_local ]; then
