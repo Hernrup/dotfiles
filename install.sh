62a86823aa849a4d5d2e9265799504b3a8a6fc75
Mikael Hernrup <mikael@hernrup.se>
2018-06-18 12:49:55 +0200

    rename dotted folders

--- install.sh
+++ install.sh
@@ -8,12 +8,12 @@ ln -sbfv $SCRIPTPATH/.agignore ~/.agignore
 ln -sbfv $SCRIPTPATH/.vimrc ~/.vimrc
 ln -sbfv $SCRIPTPATH/.minttyrc ~/.minttyrc
 ln -sbfv $SCRIPTPATH/.tmux.conf ~/.tmux.conf
-ln -sbfv $SCRIPTPATH/.vim ~/.vim
-ln -sbfv $SCRIPTPATH/.vim ~/.config/nvim
+ln -sbfv $SCRIPTPATH/vim ~/.vim
+ln -sbfv $SCRIPTPATH/vim ~/.config/nvim
 ln -sbfv $SCRIPTPATH/bin ~/bin
 ln -sbfv $SCRIPTPATH/.zshrc ~/.zshrc
 ln -sbfv $SCRIPTPATH/.pdbrc ~/.pdbrc
-ln -sbfv $SCRIPTPATH/.zsh ~/.zsh
+ln -sbfv $SCRIPTPATH/zsh ~/.zsh
 ln -sbfv $SCRIPTPATH/.ctags ~/.ctags
 
 if [ ! -f ~/.gitconfig_local ]; then
